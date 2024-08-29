6cf890c2288e1f864646431269821c309b8cb232
Татьяна Горянина <tanya200898@gmail.com>
2024-08-29 22:43:47 +0300

    init: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('')
+  for (let i=0; i<arr.length; i++) {
+    for (let configs of bracketsConfig) {
+      if (arr[i] === configs[1] && arr[i-1] === configs[0]) {
+        arr.splice(--i,2)
+        i--
+      }      
+    } 
+  }
+  return arr.length>0 ? false : true
 }
